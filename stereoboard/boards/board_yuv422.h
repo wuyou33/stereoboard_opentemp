5bce6148a0244890307294fdcf7c6dba02a4ed2f
dewagter <dewagter@gmail.com>
2015-06-20 14:57:51 +0200

    simply stream a the raw color stream

---
+++ stereoboard/boards/board_yuv422.h
@@ -0,0 +1,4 @@
+#define USART4_BAUD 1000000
+#define SEND_IMAGE 1
+#define CAPTURE_MODE_SNAPSHOT 1 // We want slow good-looking frames: it does not matter if we have to wait a bit longer as long as
+#define CAMERA_CPLD_STEREO camera_cpld_stereo_linemux
