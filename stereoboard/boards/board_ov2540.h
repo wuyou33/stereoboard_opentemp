5e8d94941c1d3733c0ac6e52fccf5ec8351a8995
dewagter <dewagter@gmail.com>
2015-06-21 20:22:03 +0200

    Linux camera drivers

---
+++ stereoboard/boards/board_ov2540.h
@@ -0,0 +1,11 @@
+
+
+#define USART4_BAUD 9600 // high baudrate necessary for sending images / disparity maps.
+//#define SEND_IMAGE 1
+
+#define DCMI_TEN_BITS 1
+
+#define IMAGE_WIDTH 160
+#define IMAGE_HEIGHT 120
+
+#define USE_OV2640
---
+++ stereoboard/boards/board_ov7670.h
@@ -0,0 +1,17 @@
+
+
+#define USE_USART4
+#define UsartTx Usart4Tx
+#define USART4_BAUD 1000000 // high baudrate necessary for sending images / disparity maps.
+
+#define SEND_MATRIX 0
+#define SEND_DISPARITY_MAP 0
+#define SEND_IMAGE 1
+
+
+#define DCMI_TEN_BITS 1
+
+#define IMAGE_WIDTH 160
+#define IMAGE_HEIGHT 120
+
+#define USE_OV7670
