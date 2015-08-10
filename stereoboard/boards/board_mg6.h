c2091d68be303ee115ef1c2ca10564f915e1076e
Roland Meertens <rolandmeertens@gmail.com>
2015-08-10 15:43:26 +0200

    Set to snapshot mode

--- stereoboard/boards/board_mg6.h
+++ stereoboard/boards/board_mg6.h
@@ -5,3 +5,4 @@
 #define USE_USART1
 #define UsartTx Usart1Tx
 #define USART1_BAUD 1000000 // high baudrate necessary for sending images / disparity maps.
+#define CAPTURE_MODE_SNAPSHOT 1   // snapshot! Mostly for debugging
