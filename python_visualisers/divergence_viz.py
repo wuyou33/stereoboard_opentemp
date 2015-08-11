95c3d5b64b065f0f6a90c0f97fb76e4e71ae3e31
k.n.mcguire@tudelft.nl <k.n.mcguire@tudelft.nl>
2015-08-11 18:15:05 +0200

    changed python divergence_viz.py to calculate velocity from displacement

--- python_visualisers/divergence_viz.py
+++ python_visualisers/divergence_viz.py
@@ -4,20 +4,26 @@ import stereoboard_tools
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-plt.axis([0,300, 0, 200])
+import math
+plt.axis([0,300, -0.5,0.5])
 plt.ion()
 plt.show()
    
 ser = serial.Serial('/dev/ttyUSB0',1000000,timeout=None)
-frameNumber = 0
+height = 0
+radperpx = 0
+distance_pinhole = 0.03
+angle_disp = 0
 saveImages= False
 currentBuffer=[]
+FOV_x=57.4
+FOV_y=45
 maxExpectedInImage=250 # Each pixel is divided by this value to go to grayscale values for the cv2 image
 cv2.namedWindow('img',cv2.WINDOW_NORMAL)
-yint_yHistory=[]
-yint_xHistory=[]
-slope_xHistory=[]
-slope_yHistory=[]
+velocity_x=0
+velocity_y=0
+velocity_xHistory=[]
+velocity_yHistory=[]
 while True:
     try:
         # Read the image
@@ -39,26 +45,35 @@ while True:
             img = stereoboard_tools.fill_image_array(sync1,oneImage, lineLength, lineCount)
 	    print 'img: ' , img
             img=np.array(img)
-	    slope_x=img[0,0]
-            yint_x=img[0,1]
-	    slope_y=img[0,2]
-            yint_y=img[0,3]
-            frame_number=img[0,4]
-           # print 'x:: slop: ' , slope_x, ' yint: ' , yint_x,'x:: slop: ' , slope_y, ' yint: ' , yint_y
-            print frame_number
-	    print img
-            yint_xHistory.append(yint_x)
-            yint_yHistory.append(yint_y)
-            slope_xHistory.append(slope_x)
-            slope_yHistory.append(slope_y)
-	    plt.plot(yint_xHistory)
+	    slope_x=(img[0,0]-100)/1000
+            yint_x=(img[0,1]-100)/100
+	    slope_y=(img[0,2]-100)/1000
+            yint_y=(img[0,3]-100)/100
+            height=img[0,4]
+            
+            radperpx=(FOV_x*math.pi/180)/128
+            angle_disp=height/2*radperpx
+            height_meters=distance_pinhole/math.tan(angle_disp);
+            
+            radperpx=(FOV_x*math.pi/180)/128
+            angle_disp=yint_x*radperpx
+            velocity_x=height_meters*math.tan(angle_disp)*25
+
+            radperpx=(FOV_y*math.pi/180)/96
+            angle_disp=yint_y*radperpx
+            velocity_y=height_meters*math.tan(angle_disp)*25
+            print velocity_x
+            print velocity_y
+            print height_meters
+
+
+            velocity_xHistory.append(velocity_x)
+            velocity_yHistory.append(velocity_y)
+	    plt.plot(velocity_xHistory)
 	    plt.draw()
-	   # plt.plot(slope_xHistory)
-	    #plt.draw()
-	   # plt.plot(yint_yHistory)
-	    #plt.draw()
-	    #plt.plot(slope_yHistory)
-	    #plt.draw()
+            plt.plot(velocity_yHistory)
+	    plt.draw()
+
 	    time.sleep(0.05)
         
 
