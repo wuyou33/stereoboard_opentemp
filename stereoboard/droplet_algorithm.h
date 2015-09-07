083c3f9b642af89b62f056ca0d2b87915349afda
sjoerdtijmons <sjoerdtijmons@gmai.com>
2015-09-07 11:22:59 +0200

    stereo vision for quadrotor demo added + updates for droplet code

--- stereoboard/droplet_algorithm.h
+++ stereoboard/droplet_algorithm.h
@@ -14,13 +14,13 @@
 
 
 // parameter setting
-int obst_thr_1 = 10; 		// obstacle threshold for phase 1
-int obst_wait_2 = 3000; 	// wait time for phase 2
-int obst_thr_3 = 10;		// obstacle threshold for phase 3
-int obst_thr_4 = 10;		// obstacle threshold for phase 4
-int obst_wait_4 = 500;		// wait time for phase 4
+int obst_thr_1 = 7; 			// obstacle threshold for phase 1
+uint32_t obst_wait_2 = 1800; 	// wait time for phase 2
+int obst_thr_3 = 10;			// obstacle threshold for phase 3
+int obst_thr_4 = 10;			// obstacle threshold for phase 4
+int obst_wait_4 = 500;			// wait time for phase 4
 
-int obst_cons_1 = 5;		// obstacle consistency threshold for phase 1
+int obst_cons_1 = 3;		// obstacle consistency threshold for phase 1
 int obst_cons_3 = 1;		// no-obstacle consistency threshold for phase 3
 int obst_cons_5 = 2;		// obstacle consistency threshold for phase 4
 
@@ -28,15 +28,15 @@ int obst_cons_5 = 2;		// obstacle consistency threshold for phase 4
 int obst_count_1 = 0;		// counter for sequential obstacle detections in phase 1
 int obst_free_3 = 0;		// counter for sequential no-obstacle detections in phase 3
 int obst_dect_4 = 0;			// counter for sequential obstacle detections in phase 4
-uint64_t obst_time = 0; 			// timer for phase 2
-uint64_t obst_time_4 = 0;			// timer for phase 4
+uint32_t obst_time = 0; 			// timer for phase 2 and 4
 
 int phase = 1;
-uint64_t internal_time = 0;
 uint32_t prev_time = 0;
+uint32_t passed_time = 0;
+uint32_t clock_period = 20000;
 
 
-uint16_t run_droplet_algorithm(int disparities_high, uint64_t sys_time );
+uint16_t run_droplet_algorithm(int disparities_high, volatile uint32_t sys_time );
 
 
 
