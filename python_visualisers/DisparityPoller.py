9e9aacd0fcb7c4050db56c23f480ce20efbc45ef
rmeertens <rolandmeertens@gmail.com>
2015-09-10 15:57:00 +0200

    +frame number

--- python_visualisers/DisparityPoller.py
+++ python_visualisers/DisparityPoller.py
@@ -1,21 +1,18 @@
-import cv2
 import serial
 import stereoboard_tools
-import Tkinter as tk
 import numpy as np
-import csv
 import time
 
 class DisparityPoller:
     """Polls the stereoboard"""
     def pollStereoboard(self):
         # Read the image
-        currentBuffer, location,endOfImagesFound = stereoboard_tools.readPartOfImage(self.ser, self.currentBuffer)
+        self.currentBuffer, location,endOfImagesFound = stereoboard_tools.readPartOfImage(self.ser, self.currentBuffer)
         startPosition=location[0]
         endPosition=location[1]
         if location[0] > -1:
-            oneImage = currentBuffer[startPosition:endPosition]
-            currentBuffer=currentBuffer[endPosition::]
+            oneImage = self.currentBuffer[startPosition:endPosition]
+            self.currentBuffer=self.currentBuffer[endPosition::]
             
             
             # Search the startbyte
@@ -29,10 +26,13 @@ class DisparityPoller:
             img = stereoboard_tools.fill_image_array(sync1,oneImage, lineLength, lineCount)
             self.lastImg=np.array(img)
             self.lastImgTimestamp=time.time()
+            self.lastFrame += 1
     def __init__(self, serialPortName, baudRate,printUpdates=False):
         self.ser = serial.Serial(serialPortName,baudRate,timeout=None)
         self.currentBuffer=[]
+        self.lastFrame = 0
         self.lastImgTimestamp=None
         self.printUpdates=printUpdates
+        self.lastImg=[]
 
 
