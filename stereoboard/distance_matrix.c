81c87402465721a82750154c64f92b69bf766f9a
guidoAI <guido.de.croon@gmail.com>
2015-11-13 13:49:46 +0100

    Disparity matrix now iterates over all subpixel disparity values, variable names changed to correspond to content

--- stereoboard/distance_matrix.c
+++ stereoboard/distance_matrix.c
@@ -1,6 +1,8 @@
 /*
  * distance_matrix.c
  *
+ * Makes a matrix of widthPerBin x heightPerBin with the COUNTER_THRESHOLD-th disparity value.
+ *
  *  Created on: Jul 6, 2015
  *      Author: roland
  */
@@ -14,74 +16,57 @@ void calculateDistanceMatrix(uint8_t *disparity_image,
                              uint8_t heightPerBin, uint8_t *toSendBuffer, uint32_t disparity_range)
 {
 
-  //int indexBuffer;
-
   uint8_t y;
   uint8_t valueInImageBuffer = 0;
   uint16_t positionInImageBuffer = 0;
   uint8_t positionInMatrix = 0;
   uint8_t x;
   //uint8_t z;
-  uint8_t highestValues[MATRIX_WIDTH_BINS * MATRIX_HEIGHT_BINS][5];
-  uint16_t sumDisparities[MATRIX_WIDTH_BINS * MATRIX_HEIGHT_BINS][disparity_range * RESOLUTION_FACTOR];
+
+  uint16_t disparityHistogram[MATRIX_WIDTH_BINS * MATRIX_HEIGHT_BINS][disparity_range * RESOLUTION_FACTOR];
   for (x = 0; x < MATRIX_WIDTH_BINS * MATRIX_HEIGHT_BINS; x++) {
-    for (y = 0; y < 5; y++) {
-      highestValues[x][y] = 0;
-    }
     for (y = 0; y < disparity_range * RESOLUTION_FACTOR; y++) {
-      sumDisparities[x][y] = 0;
+      disparityHistogram[x][y] = 0;
     }
   }
 
+  // Fill the disparity histogram:
   for (x = 0; x < MATRIX_WIDTH_BINS; x++) {
     for (y = 0; y < MATRIX_HEIGHT_BINS; y++) {
       int line;
       for (line = 0; line < heightPerBin; line++) {
         int bufferIndex = 0;
-        for (bufferIndex = 0; bufferIndex < widthPerBin;
-             bufferIndex++) {
+        for (bufferIndex = 0; bufferIndex < widthPerBin; bufferIndex++) {
           positionInImageBuffer = pixelsPerLine * (y * heightPerBin) + line * pixelsPerLine + widthPerBin * x + blackBorderSize +
                                   bufferIndex;
           valueInImageBuffer = disparity_image[positionInImageBuffer];
 
           positionInMatrix = y * MATRIX_WIDTH_BINS + x;
 
-          sumDisparities[positionInMatrix][valueInImageBuffer]++;
-
-          /*
-          if(valueInImageBuffer>matrixBuffer[positionInMatrix])
-          {
-            matrixBuffer[positionInMatrix]=valueInImageBuffer;
-          }
-          for(z=0;z <5;z++)
-          {
-            if(valueInImageBuffer>highestValues[positionInMatrix][z])
-            {
-              highestValues[positionInMatrix][z]=valueInImageBuffer;
-              break;
-            }
-          }
-          */
+          disparityHistogram[positionInMatrix][valueInImageBuffer]++;
         }
       }
     }
   }
 
 
-  // Average by dividing by the amount of pixels per bin
+  // go over the cells in the matrix and determine per cell what the n-th disparity value is
+  // n = COUNTER_THRESHOLD
+  int COUNTER_THRESHOLD = 10;
   int bufferIndex;
-  int anyOn = 0;
+  int anyOn = 0; // whether any cell in the matrix has a "close" object
+  int disp;
 
-  for (bufferIndex = 0; bufferIndex < MATRIX_WIDTH_BINS * MATRIX_HEIGHT_BINS;
-       bufferIndex++) {
-
-    int sum_disparities = 0;
-    for (y = (disparity_range - 1) * RESOLUTION_FACTOR; y >= 0; y--) {
-      int COUNTER_THRESHOLD = 10;
-      sum_disparities += sumDisparities[bufferIndex][y];
-      if (sum_disparities > COUNTER_THRESHOLD) {
-        toSendBuffer[bufferIndex] = y;
-        if (y > CLOSE_BOUNDARY) {
+  // go over all matrix cells
+  for (bufferIndex = 0; bufferIndex < MATRIX_WIDTH_BINS * MATRIX_HEIGHT_BINS; bufferIndex++) {
+    int n_disparities = 0;
+    // go over all disparities
+    for (disp = disparity_range * RESOLUTION_FACTOR - 1; disp >= 0; disp--) {
+      n_disparities += disparityHistogram[bufferIndex][disp];
+      // after n_disparities, we consider that the measurements are no longer noise
+      if (n_disparities > COUNTER_THRESHOLD) {
+        toSendBuffer[bufferIndex] = disp;
+        if (disp > CLOSE_BOUNDARY) {
           anyOn = 1;
         }
         break;
