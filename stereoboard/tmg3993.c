965f7b69e164e3e5c82fafe1eb245ab69132f658
kevinlamers <kevin.lamers@gmail.com>
2016-01-27 18:26:19 +0100

    Cleaned up filter for proximity sensor

--- stereoboard/tmg3993.c
+++ stereoboard/tmg3993.c
@@ -7,8 +7,8 @@
 #include "tmg3993.h"
 #include "i2c.h"
 
-#define prox_len 50
-#define angle_len 50
+#define prox_len 100 //50
+#define angle_len 100 //50
 
 int16_t prox[prox_len];
 int16_t prox_east[prox_len];
@@ -18,6 +18,16 @@ int16_t prox_filt = 0;
 int16_t prox_east_filt;
 int16_t prox_west_filt;
 int16_t angle_filt;
+
+int32_t prox_idx = 0;
+int32_t prox_east_idx = 0;
+int32_t prox_west_idx = 0;
+int32_t angle_idx = 0;
+
+int32_t prox_sum = 0;
+int32_t prox_east_sum = 0;
+int32_t prox_west_sum = 0;
+int32_t angle_sum = 0;
 
 void TMG3993_Init(void)
 {
@@ -34,25 +44,20 @@ int16_t TMG3993_Read_Proximity(void)
 {
   uint8_t response;
   I2CRead(ADDRESS_TMG3993, REGISTER_PDATA, &response);
-  prox[prox_len-1] = response;
 
   // Moving average on proximity data
-  int i;
-  for (i=0;i<prox_len-1;i++){
-    prox[i] = prox[i+1];
-  }
-  uint16_t prox_sum = 0;
-  for (i=0;i<prox_len;i++){
-    prox_sum = prox_sum + prox[i];
-  }
-  prox_filt = prox_sum/(prox_len);
+  prox_sum = prox_sum - prox[prox_idx%prox_len];
+  prox[prox_idx%prox_len] = response;
+  prox_idx++;
+  prox_sum = prox_sum + response;
+  prox_filt = prox_sum/prox_len;
 
   return prox_filt;
 }
 
 int16_t TMG3993_Read_Proximity_East(void)
 {
-
+  //I2CWrite(ADDRESS_TMG3993, REGISTER_WAITTIME,COMMAND_WTIME_1);
   I2CWrite(ADDRESS_TMG3993, REGISTER_CONFIG3,COMMAND_Only_East);
 
   uint8_t status;
@@ -63,27 +68,19 @@ int16_t TMG3993_Read_Proximity_East(void)
 
   uint8_t response;
   I2CRead(ADDRESS_TMG3993, REGISTER_PDATA, &response);
-  //prox_east[prox_len-1] = response*100+300+60; //compensate for offset between east and west sensor
-  prox_east[prox_len-1] = response*100 + prox_filt*4 + 260;
 
   I2CWrite(ADDRESS_TMG3993, REGISTER_CONFIG3,COMMAND_All_Sensors);
-
+  //I2CWrite(ADDRESS_TMG3993, REGISTER_WAITTIME,COMMAND_WTIME_220);
 
   // Moving average on proximity data
-  int i;
-  for (i=0;i<prox_len-1;i++){
-    prox_east[i] = prox_east[i+1];
-  }
-  uint32_t prox_east_sum = 0;
-  for (i=0;i<prox_len;i++){
-    prox_east_sum = prox_east_sum + prox_east[i];
-  }
-  prox_east_filt = prox_east_sum/(prox_len);
+  response = response + 1 + (prox_filt/25);
+  prox_east_sum = prox_east_sum - prox_east[prox_east_idx%prox_len];
+  prox_east[prox_east_idx%prox_len] = response;
+  prox_east_idx++;
+  prox_east_sum = prox_east_sum + response;
+  prox_east_filt = prox_east_sum/prox_len;
 
-
- // prox_east_filt = prox_east_filt + prox_filt*305/10000 + 1;//+4
-
-  return prox_east_filt; //response + prox[prox_len-1]*305/10000 + 4;
+  return prox_east_filt;
 }
 
 int16_t TMG3993_Read_Proximity_West(void)
@@ -99,21 +96,16 @@ int16_t TMG3993_Read_Proximity_West(void)
 
   uint8_t response;
   I2CRead(ADDRESS_TMG3993, REGISTER_PDATA, &response);
-  prox_west[prox_len-1] = response*100;
 
   I2CWrite(ADDRESS_TMG3993, REGISTER_CONFIG3,COMMAND_All_Sensors);
   //I2CWrite(ADDRESS_TMG3993, REGISTER_WAITTIME,COMMAND_WTIME_220);
 
   // Moving average on proximity data
-  int i;
-  for (i=0;i<prox_len-1;i++){
-    prox_west[i] = prox_west[i+1];
-  }
-  uint32_t prox_west_sum = 0;
-  for (i=0;i<prox_len;i++){
-    prox_west_sum = prox_west_sum + prox_west[i];
-  }
-  prox_west_filt = prox_west_sum/(prox_len);
+  prox_west_sum = prox_west_sum - prox_west[prox_west_idx%prox_len];
+  prox_west[prox_west_idx%prox_len] = response;
+  prox_west_idx++;
+  prox_west_sum = prox_west_sum + response;
+  prox_west_filt = prox_west_sum/prox_len;
 
   return prox_west_filt;
 }
@@ -123,18 +115,16 @@ int16_t TMG3993_Read_Angle(void)
   //I2CWrite(ADDRESS_TMG3993, REGISTER_WAITTIME,COMMAND_WTIME_1);
 
 	// Calculate angle between sensor and obstacle
-	angle[angle_len-1] = (int)((prox_east_filt-prox_west_filt)/(prox_filt*0.001778*100));
+	//angle[angle_len-1] = (int)((prox_east_filt-prox_west_filt)/(prox_filt*0.001778*100));
+  int16_t response = (int)((prox_east_filt-prox_west_filt)/(prox_filt*0.001778*100));
 
   // Moving average on proximity data
-	int i;
-  for (i=0;i<angle_len-1;i++){
-    angle[i] = angle[i+1];
-  }
-  int32_t  angle_sum = 0;
-  for (i=0;i<angle_len;i++){
-    angle_sum = angle_sum + angle[i];
- }
-  angle_filt = angle_sum/(angle_len);
+	angle_sum = angle_sum - angle[angle_idx%angle_len];
+	angle[angle_idx%angle_len] = response;
+	angle_idx++;
+	angle_sum = angle_sum + response;
+	angle_filt = angle_sum/angle_len;
+
 
   //I2CWrite(ADDRESS_TMG3993, REGISTER_WAITTIME,COMMAND_WTIME_220);
   return angle_filt;//angle[prox_len-1];//angle_filt;
