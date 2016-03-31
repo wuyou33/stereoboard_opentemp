ce211db81f797ac034fed1e7c8b630e9b5b97225
Roland Meertens <rolandmeertens@gmail.com>
2016-03-31 12:02:49 +0200

    changed hor board 33

--- stereoboard/boards/board_33.h
+++ stereoboard/boards/board_33.h
@@ -1,7 +1,7 @@
 #define BOARD_NUMBER  0
 
-#define DISPARITY_OFFSET_LEFT  5    // pixel offset left of DISPARITY_BORDER
-#define DISPARITY_OFFSET_RIGHT  5   // pixel offset right of DISPARITY_BORDER
+#define DISPARITY_OFFSET_LEFT  4    // pixel offset left of DISPARITY_BORDER
+#define DISPARITY_OFFSET_RIGHT  4   // pixel offset right of DISPARITY_BORDER
 #define DISPARITY_BORDER  96		    // width threshold for using offset left and right
-#define DISPARITY_OFFSET_HORIZONTAL -5
+#define DISPARITY_OFFSET_HORIZONTAL -24
 #define RESOLUTION_FACTOR 6
