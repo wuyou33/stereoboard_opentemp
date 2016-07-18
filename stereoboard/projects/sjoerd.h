d945dd4d260b16385f357df63e0566959cd78541
sjoerdtijmons <sjoerdtijmons@gmai.com>
2016-07-18 12:15:23 +0200

    just changed own project

--- stereoboard/projects/sjoerd.h
+++ stereoboard/projects/sjoerd.h
@@ -11,16 +11,18 @@
 /*****************
  * Project parameters
  *****************/
-#define DEFAULT_BOARD_FUNCTION SEND_ROTATIONS
+#define DEFAULT_BOARD_FUNCTION SEND_DISPARITY_MAP//SEND_IMAGE
 #define CAMERA_CPLD_STEREO camera_cpld_stereo_pixmux
+#define CAPTURE_MODE_SNAPSHOT 1
 
 //////////////////////////////////////////////////////
 // Settings
 #define MAX_RATIO 10 // 10
 #define STEREO_ALGORITHM 0 // 1 = Dense   0 = Sparse
 //#define LARGE_IMAGE
-#define ODOMETRY 1
+//#define ODOMETRY 1
 
+#define USART4_BAUD 2000000
 
 
 
