b60e63c48ad68f96a84a858460d189e9ad25e800
guidoAI <guido.de.croon@gmail.com>
2016-09-15 21:07:02 +0200

    Population is now half initialized with the previous population and half with a new guess. The initial guess for the position plus an average disparity is now sent in a message. Some bugs were removed from gate_detection_fp.

--- stereoboard/gate_detection.h
+++ stereoboard/gate_detection.h
@@ -16,11 +16,11 @@
 #define MAX_POINTS 250
 
   // main gate detection function:
-  void gate_detection(struct image_i* disparity_image, float* x_center, float* y_center, float* radius, float* fitness, int initialize_fit_with_pars, int min_sub_disparity);
+  void gate_detection(struct image_i* disparity_image, float* x_center, float* y_center, float* radius, float* fitness, float* x0, float* y0, float* size0, int min_sub_disparity);
 
 	// "private" functions:
 	void convert_disparitymap_to_points(struct image_i* disparity_image, int min_sub_disparity);
-	void fit_window_to_points(float* x0, float* y0, float* size0, float* fitness);
+	void fit_window_to_points(float* x0, float* y0, float* size0, float* x_center, float* y_center, float* radius, float* fitness);
 	float mean_distance_to_circle(float* genome);
   float get_outlier_ratio(float* genome, float total_sum_weights);
 	float mean_distance_to_square(float* genome);
