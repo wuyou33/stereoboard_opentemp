fb2f7a7dac7c51920e45f8236163da995713fd0b
kirkscheper <kirkscheper@gmail.com>
2016-09-23 18:38:06 +0200

    update calibrationof board 33

--- stereoboard/boards/board_33.h
+++ stereoboard/boards/board_33.h
@@ -3,5 +3,5 @@
 #define DISPARITY_OFFSET_LEFT  4    // pixel offset left of DISPARITY_BORDER
 #define DISPARITY_OFFSET_RIGHT  4   // pixel offset right of DISPARITY_BORDER
 #define DISPARITY_BORDER  96		    // width threshold for using offset left and right
-#define DISPARITY_OFFSET_HORIZONTAL -24
+#define DISPARITY_OFFSET_HORIZONTAL 3
 #define RESOLUTION_FACTOR 6
