dbd7a2461ec556689b966c86d3afb1bac4f857b9
kirkscheper <kirkscheper@gmail.com>
2016-09-23 17:57:09 +0200

    added new stereo board

---
+++ stereoboard/boards/board_40.h
@@ -0,0 +1,7 @@
+#define BOARD_NUMBER  0
+
+#define DISPARITY_OFFSET_LEFT  4    // pixel offset left of DISPARITY_BORDER
+#define DISPARITY_OFFSET_RIGHT  6   // pixel offset right of DISPARITY_BORDER
+#define DISPARITY_BORDER  75		    // width threshold for using offset left and right
+#define DISPARITY_OFFSET_HORIZONTAL -2 
+#define RESOLUTION_FACTOR 6
