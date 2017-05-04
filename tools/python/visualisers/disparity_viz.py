4352fb1c94b8551fcd563f91300ebab4b358fa8d
kirkscheper <kirkscheper@gmail.com>
2017-05-04 16:26:12 +0200

    update disarity_viz

--- tools/python/visualisers/disparity_viz.py
+++ tools/python/visualisers/disparity_viz.py
@@ -12,9 +12,9 @@ import time
 
 if len(sys.argv) > 1:
   print(sys.argv[1])
-  ser = serial.Serial(sys.argv[1],1000000,timeout=None)
+  ser = serial.Serial(sys.argv[1],921600,timeout=None)
 else:
-  ser = serial.Serial('/dev/ttyUSB0',1000000,timeout=None)
+  ser = serial.Serial('/dev/ttyUSB0',921600,timeout=None)
   
 frameNumber = 0
 saveImages= False
@@ -22,18 +22,9 @@ treshold=0.3
 max_time = 500;
 
 currentBuffer=[]
-print cv2.__version__
-if '3.0.0'==cv2.__version__:
-    cv2.namedWindow('img',cv2.WINDOW_NORMAL)
-if '3.0.0-dev'==cv2.__version__:
-    cv2.namedWindow('img',cv2.WINDOW_NORMAL)
-
-
-fileToWrite=file("data.csv",'w')
-dataWriter=csv.writer(fileToWrite)
-
 now = time.clock()
-            
+cv2.namedWindow('stereoimg',cv2.WINDOW_NORMAL)
+
 # main loop:
 while True:
     try:
@@ -45,37 +36,36 @@ while True:
         if endOfImagesFound > 0:
             oneImage = currentBuffer[startPosition:endPosition]
             currentBuffer = currentBuffer[endPosition::]
-
-            # Search the startbyte
-            sync1, length,lineLength, lineCount=stereoboard_tools.determine_image_and_line_length(oneImage)
-            print 'length: ', length, ' count: ', lineCount, ' lineLength: ', lineLength, ' end of images found: ', endOfImagesFound
-            if sync1<0:    # We did not find the startbit... try again
-                continue
-
-
-            img = stereoboard_tools.fill_image_array(sync1,oneImage, lineLength, lineCount)
-            img = np.array(img)
-
-            if saveImages:
-                import scipy
-                fileNameBoth = 'image'+str(frameNumber)+'.png'
-                scipy.misc.imsave(fileNameBoth, img)
-            totalData=[frameNumber,time.time()]
-            img /= 20   # disparity_max
-            #img /= 6
             
-            print 1 / (time.clock() - now)
-            now = time.clock()
+            # Search the startbyte
+            sync1, length, lineLength, lineCount=stereoboard_tools.determine_image_and_line_length(oneImage)
+            #print 'length: ', length, ' count: ', lineCount, ' lineLength: ', lineLength, ' end of images found: ', endOfImagesFound
+            if sync1 < 0:    # We did not find the startbit... try again
+                continue
+                
+            img = stereoboard_tools.fill_image_array(sync1, oneImage, lineLength, lineCount)
+            print img.size
+            if img.size <= 32:
+              print img
+              count = 0
+            elif img.size >= 12288:
+              if saveImages:
+                  import scipy
+                  fileNameBoth = 'image'+str(frameNumber)+'.png'
+                  scipy.misc.imsave(fileNameBoth, img)
+              totalData=[frameNumber,time.time()]
+              img *= 2
+              
+              #print 1 / (time.clock() - now)
+              now = time.clock()
 
-            # Create a color image
-            img = stereoboard_tools.createRedBlueImage(img,lineCount,lineLength)
-            img = img[::2]
-            # if (not '3.0.0'==cv2.__version__) and (not '3.0.0-dev'==cv2.__version__):
-            #     print 'resizing stuff!'
-            img = cv2.resize(img,(0,0),fx=5,fy=5 ,interpolation=cv2.INTER_NEAREST)
-            cv2.imshow('img',img)
-	    	# print 'test hier'
-
+              # Create a color image
+              img = stereoboard_tools.createRedBlueImage(img,lineCount,lineLength)
+              #img = img[::2]
+              # if (not '3.0.0'==cv2.__version__) and (not '3.0.0-dev'==cv2.__version__):
+              #     print 'resizing stuff!'
+              img = cv2.resize(img,(0,0),fx=3,fy=3 ,interpolation=cv2.INTER_NEAREST)
+              cv2.imshow('stereoimg',img)
 
             key=cv2.waitKey(1)
             if 'q' == chr(key & 255):
